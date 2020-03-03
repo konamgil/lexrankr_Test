c2f9f40f160c586db66455622dc30df5d2389a6f
konamgil <rhskarlf6461@gmail.com>
2020-03-03 11:21:34 +0900

    t

--- test18.py
+++ test18.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from flask_restful import reqparse
 from lexrankr import LexRank
@@ -34,7 +34,10 @@ def post():
 
         strd = sum[1] + " " + sum[0]
 
-        return strd
+        resp = {"sum": strd}
+
+
+        return jsonify(resp)
     except Exception as e:
         return {'error': str(e)}
 
